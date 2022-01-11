5aee2a00f7357a1bff196a41ed0e00506b77c3f0
archit120 <architrungta120@gmail.com>
2022-01-11 12:04:50 +0530

    Enable cross origin access

--- main.py
+++ main.py
@@ -10,5 +10,14 @@ def hello(domain):
     except Exception as e:
         return "Error. Check domain!"
 
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
+
 if __name__== "__main__":
     app.run()
